be34372085d19d24b0bf8ad56d3e0f6b6343028e
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-07 10:12:07 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,7 +5,7 @@
 ###########################################
 ###########################################
 echo
-rm -f /tmp/$MY_IPK
+opkg remove --force-depends enigma2-plugin-extensions-gg
 wget -O /tmp/dreamsatpanel_1.3.deb "https://github.com/tarekzoka/-dreamsat/blob/main/enigma2-plugin-extensions-dreamsatpanel_1.3.deb?raw=true"
 wait
 apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
