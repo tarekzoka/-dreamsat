e29e73e2b3b891e6e90107a95ace7940e1685e74
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-07 05:13:20 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,10 +7,30 @@
 #!/bin/sh
 echo
 
-opkg install --force-overwrite  https://github.com/tarekzoka/dreamsatpanel/blob/main/enigma2-plugin-extensions-dreamsatpanel_1.3_all.ipk?raw=true
-echo ". >>>>         RESTARING     <<<<"
+wait
+#!/bin/sh
+#
+
+wget -O /tmp/dreamsatpanel_1.3.deb "https://github.com/tar1971/dreamsatpanel/blob/main/enigma2-plugin-extensions-dreamsatpanel_1.3.deb?raw=truee"
+wait
+apt-get update ; dpkg -i /tmp/*.deb ; apt-get -y -f install
+wait
+dpkg -i --force-overwrite /tmp/*.deb
+wait
+echo "================================="
+set +e
+cd ..
+wait
+rm -f /tmp/$MY_IPK
+rm -f /tmp/$MY_DEB
+	if [ $? -eq 0 ]; then
+echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
+fi
+		echo "********************************************************************************"
+echo "   UPLOADED BY  >>>>   TAREK_TT "   
+sleep 4;
+		echo ". >>>>         RESTARING     <<<<"
 echo "**********************************************************************************"
 wait
-reboot
-sleep 2;
-exit 0
+killall -9 enigma2
+exit 00
