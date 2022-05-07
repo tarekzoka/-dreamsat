cb97f44464bcedb9b5af01d3030ab2d42d07388e
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-07 10:40:39 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,7 +6,7 @@
 ###########################################
 echo
 wget -O /tmp/dreamsatpanel_1.3.deb   https://github.com/tarekzoka/-dreamsat/blob/main/enigma2-plugin-extensions-dreamsatpanel_1.3.deb?raw=true
-dpkg -i --force-overwrite /tmp/*.deb
+apt-get -f -y install
 apt-get update && dpkg -i --force-overwrite /tmp/*.deb; apt-get install -f -y
 echo ". >>>>         RESTARING     <<<<"
 echo "**********************************************************************************"
