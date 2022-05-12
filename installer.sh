15d08fbd0fe073761b56f564742449799611fb02
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2022-05-12 05:28:36 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,6 +4,7 @@
 ##
 ###########################################
 ###########################################
+#!/bin/bash
 echo
 wget -O /tmp/dreamsatpanel_1.3.deb   https://github.com/tarekzoka/-dreamsat/blob/main/enigma2-plugin-extensions-dreamsatpanel_1.3.deb?raw=true
 apt-get -f -y install
